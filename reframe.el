815ebf1c23a7fe0126a1b2b3aa27af02da3e5ec9
Dave Pearson <davep@davep.org>
2023-03-02 11:13:19 +0000

    :sparkles: Add a rule for Raven

--- reframe.el
+++ reframe.el
@@ -2,7 +2,7 @@
 ;; Copyright 2023 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.0
+;; Version: 1.1
 ;; Keywords: convenience, frames
 ;; URL: https://github.com/davep/reframe.el
 ;; Package-Requires: ((emacs "24"))
@@ -31,7 +31,9 @@
 
 (defconst reframe-rules
   '(((lambda ()
-       (and is-a-macOS-window-p (string= (downcase (system-name)) "lucien.local"))) . (100 40 300 110)))
+       (and is-a-macOS-window-p (string= (downcase (system-name)) "lucien.local"))) . (100 40 300 110))
+    ((lambda ()
+       (and is-a-macOS-window-p (string= (downcase (system-name)) "raven.local"))) . (2590 50 300 95)))
   "List of rules for reframing.")
 
 ;;;###autoload
